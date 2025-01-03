eeb7dd56a5d5bd9b1d6b4881c1e875fd00059971
diegoalfonsoc18 <dlalfonso57@gmail.com>
2025-01-02 19:35:47 -0500

    Update FinanzasGenerales.js

--- src/Screens/FinanzasGenerales.js
+++ src/Screens/FinanzasGenerales.js
@@ -1,10 +1,75 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  SafeAreaView,
+  Text,
+  FlatList,
+  TextInput,
+  StyleSheet,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+
+const data = [
+  { id: "1", title: "Estados de Resultados" },
+  { id: "2", title: "Créditos o Deudas" },
+];
 
 export default function FinanzasGenerales() {
-  return (
-    <View>
-      <Text>Hola soy FinanzasGenerales</Text>
+  const [values, setValues] = useState({});
+
+  const handleChange = (id, value) => {
+    setValues({
+      ...values,
+      [id]: value,
+    });
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{item.title}</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder="Ingrese valor"
+        value={values[item.id] || ""}
+        onChangeText={(value) => handleChange(item.id, value)}
+      />
     </View>
   );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    backgroundColor: "#2E4156",
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 18,
+    color: "#fff", // Cambia el color del texto para que sea visible sobre el fondo oscuro
+  },
+  input: {
+    height: 40,
+    borderColor: "#cc0000",
+    borderWidth: 1,
+    marginTop: 10,
+    paddingHorizontal: 10,
+    backgroundColor: "#cc0",
+    borderRadius: 5, // Agrega esta línea para redondear los bordes del input
+  },
+});
