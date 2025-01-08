c050521cf8a96a151f78c86a27f89538d8ff29b5
diegoalfonsoc18 <dlalfonso57@gmail.com>
2025-01-08 16:48:39 -0500

    Add library zustand and create store.js component

---
+++ src/util/Store.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import create from "zustand";
+
+export default function Store() {
+  return (
+    <View>
+      <Text>Store</Text>
+    </View>
+  );
+}
