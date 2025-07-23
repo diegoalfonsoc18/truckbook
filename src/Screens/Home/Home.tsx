cf0a18fa40489f5dab1cb9da11f97b1afee14ae4
Diego Alfonso <dlalfonso57@gmail.com>
2025-07-23 12:54:38 -0500

    Actualizar react-native-safe-area-context a la versión 5.5.2 y mejorar la estructura de la pantalla de inicio con ScrollView y nuevos estilos.

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { Text, Image, View, TouchableOpacity, ScrollView } from "react-native";
 import {
   SafeAreaView,
-  Text,
-  Image,
-  View,
-  TouchableOpacity,
-} from "react-native";
-import { styles } from "./HomeStyles"; // Asegúrate de que la ruta sea correcta
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
+import { styles } from "./HomeStyles";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+
 export default function Home() {
+  const insets = useSafeAreaInsets();
+
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.container} edges={["top", "left", "right"]}>
       <View style={styles.containerHeader}>
         <Image
           source={require("../../assets/icons/camionIcon.png")}
@@ -19,27 +20,39 @@ export default function Home() {
         <Text>Agrega tu camión</Text>
         <FontAwesome name="drivers-license" size={24} color="black" />
       </View>
-      <View style={styles.containerAlert}>
-        <Text>Avisos</Text>
-      </View>
-      <View style={styles.itemsContainer}>
-        <TouchableOpacity
-          style={styles.itemBox}
-          onPress={() => navigation.navigate("Multas")}>
-          <Text>Multas</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.itemBox}
-          // onPress={() => navigation.navigate("SOAT")}
-        >
-          <Text>Pago SOAT</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.itemBox}
-          // onPress={() => navigation.navigate("Licencia")}
-        >
-          <Text>Licencia conducción</Text>
-        </TouchableOpacity>
+      <View style={styles.containerScroll}>
+        <ScrollView
+          style={{ width: "100%", flex: 1 }}
+          contentContainerStyle={{
+            justifyContent: "center", // Centra verticalmente si hay espacio
+            alignItems: "center",
+            paddingBottom: 10,
+          }}
+          showsVerticalScrollIndicator={false}>
+          <View style={styles.containerAlert}>
+            <Text>Avisos</Text>
+          </View>
+          <View style={styles.itemsContainer}>
+            <TouchableOpacity style={styles.itemBox}>
+              <Text>Multas</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.itemBox}>
+              <Text>Pago SOAT</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.itemBox}>
+              <Text>Licencia conducción</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.itemBox}>
+              <Text>Técnico Mecánica</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.itemBox}>
+              <Text>Mantenimiento</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.itemBox}>
+              <Text>Seguro todo riesgo</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
--- src/Screens/Home/HomeStyles.ts
+++ src/Screens/Home/HomeStyles.ts
@@ -1,17 +1,37 @@
-import React from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../constants/colors"; // Adjust the import path as necessary
+// Define COLORS object or import it from your constants/theme file
 
 export const styles = StyleSheet.create({
   container: {
-    width: "100%",
-    height: "100%",
-    flex: 1,
-    justifyContent: "center",
+    width: "100%", // Ancho completo de la pantalla
+    backgroundColor: "#FF0B55", // Fondo principal de la pantalla
     alignItems: "center",
+    justifyContent: "space-between", // Espacio entre los elementos
+    padding: 10, // Espaciado interno
+    flex: 1, // Permite que el contenedor ocupe todo el espacio disponible
   },
   containerHeader: {
+    flex: 0.4,
     flexDirection: "row",
     width: "90%",
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    padding: 20,
+    marginTop: 20,
+    marginBottom: 20,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  headerText: {
+    fontSize: 24,
     justifyContent: "space-between",
     alignItems: "center",
     marginTop: 20,
@@ -24,35 +44,64 @@ export const styles = StyleSheet.create({
   iconHome: {
     width: 50,
     height: 50,
-    marginBottom: 10,
+    marginRight: 10,
+  },
+  containerScroll: {
+    flex: 6, // Permite que el contenedor ocupe todo el espacio disponible
+    width: "90%",
+    alignItems: "center", // Centra los elementos horizontalmente
+    backgroundColor: COLORS.surface, // Fondo del contenedor de scroll
+    marginBottom: 80,
+    borderRadius: 20, // Bordes redondeados
+    //padding: 10, // Espaciado interno
+    shadowColor: "#000",
   },
   containerAlert: {
-    width: "90%",
+    flex: 1,
+    width: "100%",
     padding: 20,
-    backgroundColor: "#fff",
     borderRadius: 10,
+    marginBottom: 20,
+    backgroundColor: COLORS.surface, // Fondo del contenedor de la fecha
+    // Sombra para iOS
     shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    // Sombra Android
+    elevation: 5,
+  },
+  itemsContainer: {
+    flex: 3, // Ocupa todo el espacio vertical disponible
+    flexWrap: "wrap", // Permite que los elementos se ajusten en varias líneas
+    flexDirection: "row", // Alinea los elementos en filas
+    justifyContent: "space-between", // Espacio entre los elementos
+    alignItems: "stretch", // Alinea los elementos para que ocupen el mismo alto// Ancho del contenedor de los elementos
+    width: "100%",
+    borderRadius: 10,
+    marginBottom: 80,
+    //        sbackgroundColor: "#CFFFE2",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
   },
-  itemsContainer: {
-    width: "90%",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
+
   itemBox: {
-    flex: 1,
-    margin: 10,
+    width: "48%",
+    margin: "1%",
+    height: 100, // Ajusta la altura según lo que necesites
+    marginVertical: 10,
     padding: 20,
-    backgroundColor: "#e0e0e0",
     borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: COLORS.surface, // Fondo del contenedor de la fecha
+    // Sombra para iOS
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    // Sombra Android
+    elevation: 5,
   },
 });
