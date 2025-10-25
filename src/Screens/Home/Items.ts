0211b1c5959a60c81122ac5403dd316c8bc62a6b
Diego Alfonso <dlalfonso57@gmail.com>
2025-10-25 06:30:43 -0500

    Fixed Home

--- src/Screens/Home/Items.ts
+++ src/Screens/Home/Items.ts
@@ -1,8 +1,21 @@
-import { StopIcon } from "../../assets/icons/icons";
+import { MultasIcon } from "../../assets/icons/icons";
+import { SoatIcon } from "../../assets/icons/icons";
+import { TecnicoMecanica } from "../../assets/icons/icons";
+
 export const items = [
   {
-    icon: StopIcon,
+    icon: MultasIcon,
     title: "Pendiente",
     subtitle: "Multas",
+  },
+  {
+    icon: SoatIcon,
+    title: "Soat",
+    subtitle: "Historial",
+  },
+  {
+    icon: TecnicoMecanica,
+    title: "Tecnomecánica",
+    subtitle: "Vencimiento",
   },
 ];
